3429f24f708db34c0a0faaa99796bfdea0cfdc14
Alex Diwa <atdiwa@gmail.com>
2019-04-16 21:04:20 +1000

    connect genre checkboxes to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,13 +8,18 @@ class BooksController < ApplicationController
     @author = params[:author]
     @title = params[:title]
     @description = params[:description]
-    Author.find_by(name: @author).books.create(title: @title, description: @description)
+
+    @new_book = Author.find_by(name: @author).books.create(title: @title, description: @description)
+    @genre_ids = params[:genres]
+    @genre_ids.each { |genre_id| @new_book.genres << Genre.find(genre_id) }
+
     redirect_to books_path
   end
 
   def new
     @authors = Author.all
     @author_names = Author.all.collect(&:name)
+    @genres = Genre.all
   end
 
   def show
